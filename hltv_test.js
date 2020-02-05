2aa2089dead1009a3c5d689496a1037c3476100d
Rishant96 <rishant.dutt@gmail.com>
2020-02-04 21:21:11 -0500

    feasibility analysis started.

--- hltv_test.js
+++ hltv_test.js
@@ -1,5 +1,9 @@
 const { HLTV } = require('hltv');
 
-HLTV.getMatches().then((res) => {
+// HLTV.getMatches().then((res) => {
+//   console.log(res.slice(0, 5))
+// })
+
+HLTV.getResults({pages: 1}).then((res) => {
   console.log(res.slice(0, 5))
 })
