77de1e850fc12059c60b0c508c44fba95c4d0f93
Rishant96 <rishant.dutt@gmail.com>
2020-02-29 22:49:08 -0500

    mysql test added

--- hltv_test.js
+++ hltv_test.js
@@ -1,9 +1,15 @@
 const { HLTV } = require('hltv');
 
-// HLTV.getMatches().then((res) => {
-//   console.log(res.slice(0, 5))
+HLTV.getMatches().then((res) => {
+  console.log(res.slice(0, 5))
+})
+
+
+
+// results = HLTV.getResults({pages: 1}).then((res) => {
+//    console.log(res.slice(0, 5))
 // })
 
-HLTV.getResults({pages: 1}).then((res) => {
-  console.log(res.slice(0, 5))
+HLTV.getEvents(3).then(res => {
+    console.log(res[0].events[0])
 })
---
+++ mysql_test.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'hltv-db-instance.c0bndg8bgr11.us-east-2.rds.amazonaws.com',
+  port     : '3306',
+  user     : 'rishant',
+  password : 'deathnoterocks96',
+  database : 'hltv_db'
+});
+
+connection.connect(function(err){
+    if(!err) {
+        console.log("Database is connected ... ");    
+        // connection.query("CREATE DATABASE hltv_db", function (err, result) {
+        //     if (err) throw err;
+        //     console.log("Database created");
+        //   });
+
+    } else {
+        console.log("Error connecting database ... ");    
+    }
+});
+
