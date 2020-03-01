8724257901d0eb39af1367d3c4ee207db554f129
Rishant96 <rishant.dutt@gmail.com>
2020-03-01 16:58:31 -0500

    method to get match stats added

--- hltv_test.js
+++ hltv_test.js
@@ -1,15 +1,20 @@
 const { HLTV } = require('hltv');
 
-HLTV.getMatches().then((res) => {
-  console.log(res.slice(0, 5))
-})
-
-
-
-// results = HLTV.getResults({pages: 1}).then((res) => {
-//    console.log(res.slice(0, 5))
+// HLTV.getMatches().then((res) => {
+//   console.log(res.slice(0, 5))
 // })
 
-HLTV.getEvents(3).then(res => {
-    console.log(res[0].events[0])
+
+
+results = HLTV.getResults({pages: 1}).then((res) => {
+   // console.log(res.slice(0, 5))
+   match_id = res[0].id
+   console.log(match_id, typeof match_id)
+   HLTV.getMatch({id: 72645}).then((match_stat) => {
+       console.log(match_stat)
+   })
 })
+
+// HLTV.getEvents(3).then(res => {
+//     console.log(res[0].events[0])
+// })
