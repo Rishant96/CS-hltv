f251973aa72bcc0c782d2f40cc42f5ee7528e600
Rishant96 <rishant.dutt@gmail.com>
2020-03-01 22:11:56 -0500

    tables created

---
+++ sql_to_create_tables.sql
@@ -0,0 +1,43 @@
+USE hltv_db;
+
+CREATE TABLE IF NOT EXISTS Map(
+    map_id INT AUTO_INCREMENT PRIMARY KEY,
+    is_ct_gavoured_side BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Team(
+    team_id INT AUTO_INCREMENT PRIMARY KEY,
+    hltv_id INT NOT NULL,
+    avg_player_rating DECIMAL(3,2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Matchup(
+    matchup_id INT PRIMARY KEY,
+    stats_id INT NOT NULL,
+    id_team_1 INT,
+    id_team_2 INT,
+    rank_team_1 INT NOT NULL,
+    rank_team_2 INT NOT NULL,
+    result CHAR(3) NOT NULL,
+    FOREIGN KEY (id_team_1)
+        REFERENCES Team (team_id)
+        ON UPDATE RESTRICT ON DELETE CASCADE,
+    FOREIGN KEY (id_team_2)
+        REFERENCES Team (team_id)
+        ON UPDATE RESTRICT ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Matchup_Map(
+    matchup_id INT,
+    map_id INT,
+    is_team1_starting_ct BOOLEAN NOT NULL,
+    half_score VARCHAR(4) NOT NULL,
+    final_score VARCHAR(5) NOT NULL,
+    PRIMARY KEY (matchup_id, map_id),
+    FOREIGN KEY (matchup_id)
+        REFERENCES Matchup (matchup_id)
+        ON UPDATE RESTRICT ON DELETE CASCADE,
+    FOREIGN KEY (map_id)
+        REFERENCES Map (map_id)
+        ON UPDATE RESTRICT ON DELETE CASCADE
+);
